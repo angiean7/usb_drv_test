f029a4e9e64a141e380cc6aa132b33358ab48b1a
angiean7 <i.anqi.angie@gmail.com>
2024-03-14 19:09:11 +0900

    2

--- usb_drv.h
+++ usb_drv.h
@@ -2,5 +2,5 @@
 #define DEV_DID                   (u16)0x0046
 #define DEV_VID                   (u16)0x1679
 
-static int dev_probe(struct usb_interface *interface, const struct usb_device_id *id) __init;
-static void dev_remove(struct usb_interface *interface) __exit;
+static int usb_probe(struct usb_interface *interface, const struct usb_device_id *id) __init;
+static void usb_remove(struct usb_interface *interface) __exit;
